e6e096900fbe3ba2f674645c188b4a09a2743928
Frank Huang <fhuang.work@gmail.com>
2014-10-25 23:40:40 +1100

    more zen

--- zen/index.js
+++ zen/index.js
@@ -4,30 +4,38 @@ function Genome(input) {
   return Zen.start(input)
     .then(Sequencing())
     .then(BCL())
-    .then(PostBCL());
+    .then(PostBCL())
+    .then(CheckPoint());
 }
 
 function Sequencing() { return Zen.mkTask('Sequencing'); }
 function BCL() { return Zen.mkTask('BCL'); }
+
 function PostBCL() {
-  function Read(n) { return Zen.mkTask('FastQC-R' + n); }
   return Zen.mkNestedTask('PostBCL',
     [Alignment(), Read(1), Read(2)]);
 }
-function Alignment() {
-  function WgsMetrics() { return Zen.mkTask('WgsMetrics'); }
-  function HsMetrics() { return Zen.mkTask('HsMetrics'); }
-  function QProfile() { return Zen.mkTask('QProfile'); }
-  function NovaSort() { return Zen.mkTask('NovaSort'); }
+function Read(n) { return Zen.mkTask('FastQC-R' + n); }
 
+function Alignment() {
   return Zen.mkNestedTask('Alignment',
     [WgsMetrics(), HsMetrics(), QProfile(), NovaSort()]);
 }
+function WgsMetrics() { return Zen.mkTask('WgsMetrics', function() {}); }
+function HsMetrics() { return Zen.mkTask('HsMetrics'); }
+function QProfile() { return Zen.mkTask('QProfile'); }
+function NovaSort() { return Zen.mkTask('NovaSort', function() { throw 'blah'; }); }
 
-Zen.run(Genome, 'data').then(function(result) {
-  console.log('Success:', JSON.stringify(result));
-}, function(err) {
-  console.err('Fail:', result);
-});
+function CheckPoint() {
+  return Zen.mkTask('CheckPoint');
+}
+
+Zen.run(Genome, 'blah').then(
+  function(result) {
+    console.log('Success:', JSON.stringify(result));
+  },
+  function(err) {
+    console.log('Fail:', err);
+  });
 
 // console.log(JSON.stringify(Zen.schema(Genome)));
--- zen/zen.js
+++ zen/zen.js
@@ -4,12 +4,12 @@ var mode = '';
 
 exports.schema = function schema(proc) {
   mode = 'schema';
-  return proc({ state: {} }).schema;
+  return proc({ states: {} }).schema;
 }
 
 exports.run = function run(proc, data) {
   mode = 'run';
-  return proc({ state: {}, data: data });
+  return proc({ states: {}, data: data });
 }
 
 exports.start = function start(input) {
@@ -27,36 +27,89 @@ exports.start = function start(input) {
   }
 }
 
-exports.mkTask = function mkTask(taskName, callback) {
-  callback = callback || function(data) { return data; };
-  if (mode === 'run') {
-    function taskFn(input) {
-      console.log('Running task ' + taskName);
-      var result = callback(input.data);
-      console.log('result:', result);
-      return { state: input.state, data: result };
-    }
-    taskFn.taskName = taskName;
-    return taskFn;
-  } else {
+function setTimeout(defer, timeout, taskName) {
+  if (timeout) {
+    setTimeout(function() {
+      defer.reject(taskName + ' timed out.');
+    }, timeout);
+  }
+}
+
+function getState(input, taskName) {
+  var state = input.states[taskName] || {};
+  input.states[taskName] = state;
+  return state;
+}
+
+function processState(input, state) {
+  var output = { states: input.states };
+  switch(state.status) {
+    case 'done':
+      return { states: input.states, data: state.data };
+    case 'running':
+    case 'failed':
+      return { states: input.states, data: '##SKIP##' };
+    default:
+      if (input.data === '##SKIP##') {
+        return { states: input.states, data: '##SKIP##' };
+      }
+  }
+}
+
+var count = 0;
+
+exports.mkTask = function mkTask(taskName, callback, timeout) {
+  if (mode === 'schema') {
     return { name: taskName };
   }
+  callback = callback || function(data) { console.log('callback', taskName); return count++; };
+
+  function taskFn(input) {
+    console.log('Running task ' + taskName, input);
+    var defer = Q.defer();
+    setTimeout(defer, timeout, taskName);
+
+    var state = getState(input, taskName);
+    var output = processState(input, state);
+    if (output) {
+      defer.resolve(output);
+    } else if (input.data !== undefined) {
+      state.status = 'running';
+      Q.resolve(input.data).then(callback)
+      .then(function(result) {
+        if (result !== undefined) {
+          state.status = 'done';
+        }
+        state.data = result;
+        input.states[taskName] = state;
+        console.log('result:', result);
+        defer.resolve({ states: input.states, data: result });
+      }, function(error) {
+        state.status = 'failed';
+        delete state.data;
+        input.states[taskName] = state;
+        defer.resolve({ states: input.states});
+      });
+    }
+    return defer.promise;
+  }
+  taskFn.taskName = taskName;
+  return taskFn;
 }
 
 exports.mkNestedTask = function mkNestedTask(taskName, tasks) {
   if (mode === 'run') {
     function taskFn(input) {
-      console.log('Running nested task ' + taskName);
+      console.log('Running nested task ' + taskName, input);
       var promises = tasks.map(function(task) {
-        return Q.resolve(input).then(task)
-        .then(function(result) { return { state: input.state, data: result }; });
+        return Q.resolve(input).then(task);
       });
       return Q.all(promises).then(function(results) {
         var resultMap = {};
         tasks.forEach(function(task, idx) {
-          resultMap[task.taskName] = results[idx];
+          resultMap[task.taskName] = results[idx].data;
         });
-        return resultMap;
+        return { states: input.states, data: resultMap };
       });
     }
     taskFn.taskName = taskName;
