673a1130d1b42d68ceeb51b029f1c797de654165
Nik Butenko <nik@butenko.me>
2014-10-26 14:38:44 +1100

    add axis, add rescale

--- js/app/chart.js
+++ js/app/chart.js
@@ -11,7 +11,7 @@
 
       $scope.y = {
         list: [
-          {name: 'bcl2fastq_Yield_(MBases)', value: 'bcl2fastq_Yield_(MBases)'},
+          {name: 'bcl2fastq_Yield_(Mbases)', value: 'bcl2fastq_Yield_(Mbases)'},
           {name: 'bcl2fastq_PCT_Q30_bases', value: 'bcl2fastq_PCT_Q30_bases'}
         ],
         value: ''
@@ -20,28 +20,8 @@
 
     }])
 
-    .factory('chartData', ['d3', '_', '$timeout', function (d3, _, $timeout) {
 
-      return function (src, metrics, dataReady) {
-        return d3.csv(src, function (data) {
-          var data1 = data.slice(0, 50);
-
-          dataReady(data1);
-
-          $timeout(function () {
-            _.each(data.slice(50, 100), function (d) {
-              data1.shift();
-              data1.push(d);
-            });
-            dataReady(data1);
-          }, 1000);
-
-        });
-      };
-
-    }])
-
-    .directive('chart', ['d3', '_', 'chartData', function (d3, _, chartData) {
+    .directive('chart', ['d3', '_', function (d3, _) {
 
 
       var _data = [];
@@ -59,55 +39,51 @@
         this.xScale.rangeRoundBands([0, this.width()], 0.1);
         this.yScale.range([this.height(), 0]);
 
+
+        var bars = this.chart.selectAll('.bar');
+
+        bars
+          .transition().duration(100)
+          .attr('width', this.xScale.rangeBand())
+          .attr('height', function (d) {
+            return this.yScale(d[this.metrics.y]);
+          }.bind(this))
+          .attr('y', function (d) {
+            return this.height() - this.yScale(d[this.metrics.y]);
+          }.bind(this))
+          .attr('x', function (d) {
+            return this.xScale(d['info_Flowcell']);
+          }.bind(this));
+
+
         this.vis.select('.x.axis')
-          .attr('transform', 'translate(0,' + (this.height()) + ')')
+          .attr('transform', 'translate(0,' + this.height() + ')')
           .call(this.xAxis)
           .selectAll('text')
+          .transition().duration(100)
           .style('text-anchor', 'end')
           .attr('dx', '-0.8em')
           .attr('dy', '0.15em');
 
+
         this.vis.select('.y.axis')
           .call(this.yAxis);
-
-        this.bar
-          .attr('transform', function (d) {
-            return 'translate(' + this.xScale(d.flowcell) + ',0)';
-          }.bind(this));
-
-        this.bar.selectAll('rect')
-          .attr('width', this.xScale.rangeBand())
-          .attr('y', function (d) {
-            return this.yScale(d.y1);
-          }.bind(this))
-          .attr('height', function (d) {
-            return this.yScale(d.y0) - this.yScale(d.y1);
-          }.bind(this));
-
-        this.legend.selectAll('rect')
-          .attr('x', this.width());
-
-        this.legend.selectAll('text')
-          .attr('x', this.width() - 6);
 
       }
 
 
       function redraw() {
         var data = _data;
-        console.log("data", data);
 
         this.xScale.domain(data.map(function (d) {
           return d['info_Flowcell'];
         }));
         this.yScale.domain(d3.extent(data, function (d) {
-          return d['bcl2fastq_PCT_Q30_bases'];
-        }));
-        console.log("this.xScale.domain()", this.xScale.domain());
-        console.log("this.yScale.domain()", this.yScale.domain());
+          return parseFloat(d[this.metrics.y]);
+        }.bind(this)));
 
 
-        var bars = this.vis.selectAll('.bar').data(data);
+        var bars = this.chart.selectAll('.bar').data(data);
 
         bars
           .transition()
@@ -115,15 +91,14 @@
           .attr('class', 'bar')
           .attr('width', this.xScale.rangeBand())
           .attr('height', function (d) {
-            return this.yScale(d['bcl2fastq_PCT_Q30_bases']);
+            return this.yScale(d[this.metrics.y]);
           }.bind(this))
           .attr('y', function (d) {
-            return this.height() - this.yScale(d['bcl2fastq_PCT_Q30_bases']);
+            return this.height() - this.yScale(d[this.metrics.y]);
           }.bind(this))
           .attr('x', function (d) {
             return this.xScale(d['info_Flowcell']);
-          }.bind(this))
-          .style('fill', 'rgba(0,100,0,0.1)');
+          }.bind(this));
 
 
         bars
@@ -134,72 +109,47 @@
           .attr('x', function (d) {
             return this.xScale(d['info_Flowcell']);
           }.bind(this))
+          .style('fill', 'rgba(0,100,0,0.1)')
 
           .transition()
           .delay(200)
           .duration(300)
           .attr('class', 'bar')
           .attr('height', function (d) {
-            return this.yScale(d['bcl2fastq_PCT_Q30_bases']);
+            return this.yScale(d[this.metrics.y]);
           }.bind(this))
           .attr('y', function (d) {
-            return this.height() - this.yScale(d['bcl2fastq_PCT_Q30_bases']);
-          }.bind(this))
-          .style('fill', 'rgba(0,0,0,0.1)');
+            return this.height() - this.yScale(d[this.metrics.y]);
+          }.bind(this));
 
         bars
           .exit()
+          .style('fill', 'rgba(255,0,0,1)')
+          .transition()
+          .duration(500)
+          .attr('height', 0)
+          .attr('y', this.height())
           .remove();
 
 
-        return;
-
-
-        this.vis.append('g')
-          .attr('class', 'x axis')
+        this.vis.select('.x.axis')
           .attr('transform', 'translate(0,' + this.height() + ')')
           .call(this.xAxis)
-
           .selectAll('text')
           .style('text-anchor', 'end')
           .attr('dx', '-0.8em')
           .attr('dy', '0.15em')
           .attr('transform', 'rotate(-65)');
 
-        this.vis.append('g')
-          .attr('class', 'y axis')
+
+        this.vis.select('.y.axis')
           .call(this.yAxis)
-          .append('text')
-          .attr('transform', 'rotate(-90)')
+          .selectAll('.label')
           .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end')
+          .attr('x', 0)
+          .attr('dy', '-1em')
+          .style('text-anchor', 'start')
           .text(this.metrics.y);
-
-
-        this.bar = this.vis.selectAll('.bar')
-          .data(data.values())
-          .enter().append('g')
-          .attr('class', 'bar')
-          .attr('transform', function (d) {
-            return 'translate(' + this.xScale(d.flowcell) + ',0)';
-          }.bind(this));
-
-        this.bar.selectAll('rect')
-          .data(function (d) {
-            return d[this.metrics.y];
-          }.bind(this))
-          .enter().append('rect')
-          .attr('width', this.xScale.rangeBand())
-          .attr('y', function (d) {
-            return this.yScale(d.y1);
-          }.bind(this))
-          .attr('height', function (d) {
-            return this.yScale(d.y0) - this.yScale(d.y1);
-          }.bind(this))
-          .style('fill', function (d) {
-            return this.color(d.name);
-          }.bind(this));
 
       }
 
@@ -212,6 +162,8 @@
 
         this.vis = this.svg.append('g')
           .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+
+        this.chart = this.vis.append('g');
 
         this.xScale = d3.scale.ordinal()
           .rangeRoundBands([0, this.width()], 0.1);
@@ -226,7 +178,23 @@
         this.yAxis = d3.svg.axis()
           .scale(this.yScale)
           .orient('left')
+          .ticks(5)
           .tickFormat(d3.format('.2s'));
+
+        this.vis.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + this.height() + ')')
+          .call(this.xAxis);
+
+        this.vis.append('g')
+          .attr('class', 'y axis')
+          .call(this.yAxis)
+          .append('text')
+          .attr('y', 6)
+          .attr('x', 0)
+          .attr('dy', '-1em')
+          .style('text-anchor', 'start')
+          .text(this.metrics.y);
 
       }
 
@@ -243,7 +211,7 @@
             y: attr.y
           },
 
-          margin: {top: 40, right: 40, bottom: 120, left: 40},
+          margin: {top: 40, right: 40, bottom: 120, left: 100},
           width: function () {
             return $scope.dimensions.width - this.margin.left - this.margin.right;
           },
@@ -255,29 +223,35 @@
         createChart.call(chart, $element.find('svg')[0]);
 
 
+        /**
+         * Mock feeding data
+         * @param {Object[]} data
+         * @param {Number} index
+         */
         function feed(data, index) {
           _.each(data.slice(index, index + 50), function (d) {
+            if (_data.length > 500) {
+              _data.shift();
+            }
             _data.push(d);
           });
           redraw.call(chart);
 
-          if (index + 50 < data.length && index + 50 < 500) {
+          if (index + 50 < data.length) {
             index = index + 50;
-            setTimeout(feed.bind(null, data, index), 1000);
+            setTimeout(feed.bind(null, data, index), 3000);
           }
         }
 
         d3.csv(attr.src, function (data) {
-          console.log("data.length", data.length, data[0]);
           feed(data, 0);
         });
 
+//        $scope.$watch('y.value.value', function (value) {
+//          chart.metrics.y = value;
+//        });
 
-        chartData(attr.src, chart.metrics, redraw.bind(chart));
-
-        $scope.$watch('y.value.value', function (value) {
-          chart.metrics.y = value;
-        });
+        $scope.$watch('dimensions', _.debounce(rescale.bind(chart), 1000), true);
 
       }
 
